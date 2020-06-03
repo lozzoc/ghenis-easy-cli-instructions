1c61e2333ca6f9e9b28b85054de590a9074049da
lozzoc <reach.maddaloc@gmail.com>
2020-06-02 23:27:25 -0700

    i thought i pushed?

--- ArchiveScript.py
+++ ArchiveScript.py
@@ -1,17 +1,10 @@
 from piazza_api import Piazza
-import json
 
 CLASS_ID="k89brrt3pq17do"
 
-with open("config.json", "r") as read_file:
-    data = json.load(read_file)
-    p = Piazza()
-    print(data["user"])
-    print(data["pass"])
-    p.user_login()
-    user_profile = p.get_user_profile()
-
-    cs290 = p.network(CLASS_ID)
-    posts = cs290.iter_all_posts(limit=50)
-    for post in posts:
-        print(post)
+p =Piazza()
+p.user_login()
+cs290 = p.network(CLASS_ID)
+posts = cs290.iter_all_posts(limit=1)
+for post in posts:
+    print(post)
